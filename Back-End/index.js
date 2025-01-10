9e4a6daffab82dc6fa9d36cf93af52e174d19301
devanshupatil <devanshupatil692@gmail.com>
2025-01-10 17:25:43 +0530

    added the cors

--- Back-End/index.js
+++ Back-End/index.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv');
 const port = process.env.PORT
 const productRouter = require('./routes/productRoutes');
 const userRouter = require('./routes/userRoutes');
+const cors = require('cors');
 
 
 // load environment variables from.env file
@@ -15,6 +16,16 @@ connectDB();
 
 // Middleware
 app.use(express.json());
+
+app.use(cors({
+  origin: [
+    'http://localhost:3000',  // Local development
+    'https://wishlist-manager-app.netlify.app',  // Production frontend
+    'https://wishlist-manager-application.onrender.com'  // If needed
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // Routes
 app.use('/api', productRouter, userRouter);
