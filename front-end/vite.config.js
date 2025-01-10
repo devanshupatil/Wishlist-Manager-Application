c56aea1ad9b3bc44b62bb338ef17199b8cad7fcc
devanshupatil <devanshupatil692@gmail.com>
2025-01-10 14:19:42 +0530

    update backend URL

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://localhost:4000'
+      '/api': 'https://wishlist-manager-application.onrender.com'
 
 
     },
