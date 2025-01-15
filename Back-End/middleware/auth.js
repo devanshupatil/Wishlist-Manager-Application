993513dba157e0842f8576d6b98f13bf7912e254
devanshupatil <devanshupatil692@gmail.com>
2025-01-15 11:37:15 +0530

    auth

--- Back-End/middleware/auth.js
+++ Back-End/middleware/auth.js
@@ -4,6 +4,7 @@ const authMiddleware = async (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
 
+    console.log("token get", authHeader);
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
       return res.status(401).json({ error: 'No token provided' });
     }
