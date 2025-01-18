b0c5e8820fe8ae722e2eddc58e060f096270110c
devanshupatil <devanshupatil692@gmail.com>
2025-01-18 16:28:44 +0530

    update routes

--- Back-End/routes/productRoutes.js
+++ Back-End/routes/productRoutes.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const {getProducts, updateProduct, deleteProduct} = require('../controller/productController');
-const { searchProduct } = require('../controller/productController');
-const { sortByDate } = require('../controller/productController');
-const { sortByPrice } = require('../controller/productController');
-const { filterByPriority } = require('../controller/productController');
 const {createProduct} = require('../controller/productController');
 
 const verifyToken  = require('../middleware/auth');
@@ -19,13 +15,7 @@ router.put('/products/:id', updateProduct);
 
 router.delete('/products/', deleteProduct);
 
-router.get('/products/search/:name', searchProduct);
 
-router.get('/products/search/:name', sortByDate);
-
-router.get('/products/search/:name', sortByPrice);
-
-router.get('/products/search/:name', filterByPriority);
 
 
 
