acd1425052f898cab1338dfbacc3c08cc72ca22d
devanshupatil <devanshupatil692@gmail.com>
2025-01-18 19:48:00 +0530

    Home Page

--- front-end/src/components/Home.jsx
+++ front-end/src/components/Home.jsx
@@ -85,7 +85,7 @@ const Home = () => {
       const response = await fetch(`${URL}/api/products/`, {
         method: 'DELETE',
         headers: {
-          // 'Content-Type': 'application/json',
+          'Content-Type': 'application/json',
           'Authorization': `Bearer ${getAccessToken()}`,
         },
         body: JSON.stringify({ id })
