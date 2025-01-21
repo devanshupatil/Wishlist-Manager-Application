2b4365f3dbb68174ac5e6cc83af38c5249fbb858
devanshupatil <devanshupatil692@gmail.com>
2025-01-21 11:33:23 +0530

    Backend

--- Back-End/routes/productRoutes.js
+++ Back-End/routes/productRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {getProducts, updateProduct, deleteProduct} = require('../controller/productController');
 const {createProduct} = require('../controller/productController');
+const {getProductById} = require('../controller/productController');
 
 const verifyToken  = require('../middleware/auth');
 
@@ -13,7 +14,11 @@ router.post('/products', createProduct);
 
 router.put('/products/:id', updateProduct);
 
-router.delete('/products/', deleteProduct);
+router.delete('/products/:id', deleteProduct);
+
+router.get('/products/:id', getProductById);
+
+
 
 
 
